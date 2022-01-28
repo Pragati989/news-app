b95da39095f7b114ac52cd45749fc280ae39e5f0
Pragati Gupta <pragatigupta989@gmail.com>
2022-01-28 20:51:45 +0530

    Video 32 - Added author, date and source

--- src/components/News.js
+++ src/components/News.js
@@ -73,7 +73,8 @@ export default class News extends Component {
       <div className ="row my-4">
         {!this.state.loading &&  this.state.articles.map((element) =>{
           return (<div className = "col-md-4" key = {element.url}>
-            <NewsItem title={element.title} description={element.description} imageUrl ={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem title={element.title} description={element.description} imageUrl ={element.urlToImage} newsUrl={element.url}
+            author ={element.author} date={element.publishedAt} source ={element.source.name}/>
           </div>);
         })}
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 export default class NewsItem extends Component {
 
   render() {
-    var {title, description, imageUrl, newsUrl} = this.props;
+    var {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className="container">
         <div className="card">
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style ={{zIndex:1, left:'50%'}}>{source}</span>
           <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text"><small>By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target ="__blank" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
